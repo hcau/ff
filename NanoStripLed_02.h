679cfd1da600002ea3b156f510e18258b2aabc95
Herve Caujolle <herve.caujolle@laposte.net>
2020-02-13 23:11:54 +0100

    Signed-off-by: Herve Caujolle <herve.caujolle@laposte.net>
    qfqdfqd

---
+++ NanoStripLed_02.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _NanoStripLed_02_H_
+#define _NanoStripLed_02_H_
+#include "Arduino.h"
+//add your includes for the project NanoStripLed_02 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project NanoStripLed_02 here
+
+
+
+
+//Do not add code below this line
+#endif /* _NanoStripLed_02_H_ */
